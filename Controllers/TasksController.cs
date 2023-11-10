ba99c2b90935e9649025219b1b39a0814db97c87
Arturs322 <krustsarturs@gmail.com>
2023-11-10 20:28:00 +0200

    add functionality to create a new task- CreateTask

--- Controllers/TasksController.cs
+++ Controllers/TasksController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using TasksManagerAPI.Data;
+using TasksManagerAPI.Models;
 
 namespace TasksManagerAPI.Controllers
 {
@@ -13,5 +14,34 @@ namespace TasksManagerAPI.Controllers
             _context = context;
         }
 
+        [HttpPost]
+        [Route("/CreateTask")]
+        public async Task<IActionResult> CreateTask(Tasks newTask)
+        {
+
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                Tasks task = new Tasks
+                {
+                    Id = Guid.NewGuid(),
+                    Title = newTask.Title,
+                };
+
+                _context.Tasks.Add(task);
+                await _context.SaveChangesAsync();
+
+                return Ok(task);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"Error in CreateTask: {ex}");
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
+        }
     }
 }
